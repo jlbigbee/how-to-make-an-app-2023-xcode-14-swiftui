3de6a3a530bf72212d9fd0a5b3ca67947f1509a7
Jonathan Bigbee <bigbee.jonathan@gmail.com>
2023-01-25 18:18:04 -0800

    Arrow Functions and Parameter Labels

--- SwiftCodePlayground.playground/Contents.swift
+++ SwiftCodePlayground.playground/Contents.swift
@@ -1,8 +1,44 @@
 import UIKit
 
-var greeting = "Hello, playground"
-
-print(greeting)
-
 var myInt:String = "5"
 print(myInt)
+
+
+func sayHello(){
+    var greeting = "Hello, playground"
+    print(greeting)
+}
+
+sayHello()
+
+
+//Function Multiple Input Syntax
+
+func doAdd(a:Int, b:Int) {
+    var sum = a + b
+    print(sum)
+}
+
+doAdd(a:3,b:2)
+
+//Arrow Function with Multiple Inputs
+
+func doAddArrow(a:Int, b:Int) -> Int {
+    var sum = a + b
+    return(sum)
+}
+
+let result = doAddArrow(a: 4, b: 2)
+
+print(result)
+
+
+//Arrow Function with Multiple Inputs and Parameter Labels
+func doAddArrows(numberone a:Int, numbertwo b:Int) -> Int {
+    var sum = a + b
+    return (sum)
+}
+
+let results = doAddArrows(numberone: <#T##Int#>, numbertwo: <#T##Int#>)
+
+
